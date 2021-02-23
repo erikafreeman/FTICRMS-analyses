a4c0346607abd8e766b91ec44257cb24901bce20
erikafreeman <ecf31@cam.ac.uk>
2021-02-23 16:09:46 +0100

    editing the stream bugs

--- 0-CleanSheet_DepthPlots.py
+++ 0-CleanSheet_DepthPlots.py
@@ -66,10 +66,11 @@ newdf['Depth'] = newdf['Depth1'].where(newdf['Slope'].str.contains('ST'), newdf[
 newdf['Depth']= newdf['Depth'].astype('str')
 newdf['Depth'] = newdf['Depth'].apply(lambda x: 'Stream' if x.startswith('T') else x)
 newdf.drop(['Slope1', 'Slope2', 'Depth1'], axis=1, inplace=True)
-newdf['Slope'] = newdf['Slope'].replace({'.1': ''}, regex=True)
-newdf['Depth'] = newdf['Depth'].replace({'.1': ''}, regex=True)
+newdf['Slope'] = newdf['Slope'].replace({'.1': '', '.2': '', '.3': ''}, regex=True)
+newdf['Depth'] = newdf['Depth'].replace({'.1': '', '.2': '', '.3': ''}, regex=True)
+newdf['Slope'].replace({"S": "1S", "B": "2B", "F": "3F", "T": '4T', 'ST':'5ST' }, inplace=True)
 print(newdf)
-newdf.to_csv('/home/erika/Desktop/likeliest_match_abspres.csv')
+#newdf.to_csv('/home/erika/Desktop/likeliest_match_abspres.csv')
 
 # fdata= SampleNames.set_index('SampleID')
 
